f5a1a71e841913f4f2605bc16d4cc4367b7faebd
nsdNite <nsd7night@gmail.com>
2024-03-11 20:14:07 +0200

    add webdriver singleton

--- main.py
+++ main.py
@@ -1,1 +1,26 @@
-from selenium.webdriver import Chrome
+from dataclasses import dataclass
+import logging
+import re
+from typing import Optional
+
+
+import requests
+from lxml.html import fromstring
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+
+# Making selenium singleton
+
+
+class WebDriverSingleTone:
+    instance: Optional[webdriver.Chrome] = None
+    options: Options = Options()
+
+    @classmethod
+    def get_instance(cls) -> webdriver.Chrome:
+        if cls.instance is None:
+            cls.options.add_argument("--headless")
+            cls.instance = webdriver.Chrome(options=cls.options)
+
+        return cls.instance
